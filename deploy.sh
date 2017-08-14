661d886a6460dff29aeb397baf9e744928357c30
pepc <pcondal@apsic.com>
2017-08-14 13:42:50 +0200

    Escaped asterisk in CloudFront invalidate command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 hugo
 s3cmd sync public/ S3://docs.xbench.net/
-aws cloudfront create-invalidation --distribution-id E4LL9AOZB3W7E --paths /*
+aws cloudfront create-invalidation --distribution-id E4LL9AOZB3W7E --paths /\*
